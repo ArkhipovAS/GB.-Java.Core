ec95e3a442677a3498c9c6afbf9b5ebfc1227519
ArkhipovAS <arhipov.sasha@gmail.com>
2020-04-07 13:26:12 +0300

    ДЗ№2

--- src/main/java/lesson2/MyArrayDataException.java
+++ src/main/java/lesson2/MyArrayDataException.java
@@ -1,4 +1,18 @@
 package lesson2;
 
-public class MyArrayDataException {
+public class MyArrayDataException extends Exception{
+    public int row;
+    public int col;
+
+
+    public MyArrayDataException(String message, int i, int j) {
+        super(message);
+        this.col = i;
+        this.row = j;
+    }
+
+    @Override
+    public String toString(){
+        return "Wrong data array \n" + "row: " + row + "\ncol: " + col;
+    }
 }
--- src/main/java/lesson2/MyArraySizeException.java
+++ src/main/java/lesson2/MyArraySizeException.java
@@ -2,15 +2,11 @@ package lesson2;
 
 import java.util.Arrays;
 
-public class MyArraySizeExeption extends Exception{
-   private String [] arr1 = new String[2];
+public class MyArraySizeException extends Exception{
 
-   public MyArraySizeExeption (String [] arr1){
-       this.arr1 = arr1;
-   }
+    public MyArraySizeException(String message) {
+        super(message);
 
-    public String[] getArr1() {
-        return arr1;
     }
 }
 
