b44397979417d2c72a48e79b29c812c0b8052fe3
ArkhipovAS <arhipov.sasha@gmail.com>
2020-04-14 12:55:28 +0700

    add hw#3

--- src/main/java/lesson3/PhoneBook.java
+++ src/main/java/lesson3/PhoneBook.java
@@ -1,4 +1,30 @@
 package lesson3;
 
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class PhoneBook {
+    public HashMap <String, HashSet<String>> pb = new HashMap<String, HashSet<String>>();
+
+
+    public HashSet<String> getPb(String name) {
+        return pb.get(name);
+    }
+
+    public void addPb(String name, String pNumber) {
+        HashSet <String> pN = new HashSet<String>();
+        pN.add(pNumber);
+        if(!this.pb.containsKey(name)){
+            this.pb.put(name, pN );
+        }
+        else {
+            this.pb.get(name).add(pNumber);
+        }
+    }
+
+    public int getSize (){
+        return this.pb.size();
+    }
+
+
 }
