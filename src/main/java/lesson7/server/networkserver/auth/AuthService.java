3bd9c2d396b2ee4c7880f8044ac6c0fa270bfef6
ArkhipovAS <arhipov.sasha@gmail.com>
2020-04-20 22:13:58 +0700

    hw#7 add private message for user

---
+++ src/main/java/lesson7/server/networkserver/auth/AuthService.java
@@ -0,0 +1,10 @@
+package lesson7.server.networkserver.auth;
+
+public interface AuthService {
+
+    String getNickByLoginAndPassword(String login, String password);
+
+    void start();
+    void stop();
+
+}
